76800e520ae35cd0ca2fdad13713585445e16b7a
Alejandro <eadelgadom@gmail.com>
2022-05-26 23:23:50 +0000

    modelo tecnicos

--- src/api/models.py
+++ src/api/models.py
@@ -17,3 +17,32 @@ class User(db.Model):
             "email": self.email,
             # do not serialize the password, its a security breach
         }
+
+class Perfil_tecnico(db.Model):
+
+    __tablename__ = 'perfil_tecnico'
+
+    id = db.Column(db.Integer, primary_key=True)
+    historial = db.Column(db.String(120), unique=True, nullable=False)
+    ubicacion = db.Column(db.String(80), unique=False, nullable=False)
+    descripcion = db.Column(db.String(120), unique=True, nullable=False)
+    apellido = db.Column(db.String(120), unique=True, nullable=False)
+    url = db.Column(db.String(120), unique=True, nullable=False)
+    id_user = db.Column(db.Integer, db.ForeignKey('user.id'))
+    user = db.relationship(User)
+    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+  
+    def __repr__(self):
+        return f'<User {self.historial}>'
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "historial": self.historial,
+            "ubicacion": self.ubicacion,
+            "descripcion": self.descripcion,
+            "url": self.url,
+            "fecha_ing": self.fecha_ing,
+            "id_user":self.id_user 
+            # do not serialize the password, its a security breach
+        }
