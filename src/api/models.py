6e15ef13cbf9706d18ba8cf5af2ca83c9b841cfe
Jack Basso <102485711+jackbasso@users.noreply.github.com>
2022-05-27 18:50:54 -0500

    Revert "modelo calificacion"

--- src/api/models.py
+++ src/api/models.py
@@ -48,7 +48,7 @@ class Falla(db.Model):
             "fecha_creacion" : self.fecha_creacion,
             "fecha_cierre" : self.fecha_cierre,
             "titulo" : self.titulo,
-            "estado" : self.estado,
+            "estado" : self,estado,
             "ubicacion" : self.ubicacion,
             "usuario" : self.id_cliente
             }
@@ -80,27 +80,5 @@ class Perfil_tecnico(db.Model):
             "id_user":self.id_user 
             # do not serialize the password, its a security breach
         }
-
-class Calificacion(db.Model):
-    __tablename__ = 'calificacion'
-    id = db.Column(db.Integer, primary_key=True)
-    calificacion = db.Column(db.String(50), nullable=False)
-    comentario = db.Column(db.String(250), nullable=False)
-    usuario_id = db.Column(Integer, ForeignKey('user.id'))
-    usuario = relationship(User)
-    propuesta_id = db.Column(Integer, ForeignKey('propuesta.id'))
-    propuesta = relationship(Propuesta)
-
-    def serialize(self):
-        return {
-            'id': self.id,           
-            'calificacion': self.calificacion,
-            'comentario': self.comentario,
-            'usuario': self.usuario,
-            'propuesta': self.propuesta
-        }
     
-
-
-
 
