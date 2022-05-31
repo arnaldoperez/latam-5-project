15a38c4aad40482711244a54ec297d3974b62b94
josemg-23 <jose.gomezt19@gmail.com>
2022-05-31 01:14:59 +0000

    third

--- src/api/admin.py
+++ src/api/admin.py
@@ -1,7 +1,7 @@
   
 import os
 from flask_admin import Admin
-from .models import db, User, Perfil_tecnico
+from .models import db, User, Perfil_tecnico, falla
 from flask_admin.contrib.sqla import ModelView
 
 def setup_admin(app):
