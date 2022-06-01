f77c266dd9f427aa983e93ff879862117963459d
josemg-23 <jose.gomezt19@gmail.com>
2022-06-01 17:07:19 +0000

    jwt ready, falta depurar

--- src/api/models.py
+++ src/api/models.py
@@ -26,6 +26,21 @@ class User(db.Model):
             # do not serialize the password, its a security breach
         }
 
+class TokenBlockedList(db.Model):
+    id=db.Column(db.Integer, primary_key=True)
+    token=db.Column(db.String(200), unique=True, nullable=False)
+    email=db.Column(db.String(200), nullable=False, index=True)
+    created_at = db.Column(db.DateTime, nullable=False)
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "token":self.token,
+            "email":self.email,
+            "created_at":self.created_at
+        }
+      
+
 class Falla(db.Model):
 
     __tablename_ = 'falla'
