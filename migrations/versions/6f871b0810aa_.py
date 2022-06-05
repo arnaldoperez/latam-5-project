aa91c1cc764a5febc4841b04f214aa9de1f3f5c0
Alejandro <eadelgadom@gmail.com>
2022-06-05 20:43:57 +0000

    con las rutas funcionando

--- migrations/versions/6f871b0810aa_.py
+++ migrations/versions/6f871b0810aa_.py
@@ -1,8 +1,8 @@
 """empty message
 
-Revision ID: ef015387aa5a
+Revision ID: 6f871b0810aa
 Revises: 
-Create Date: 2022-06-05 20:27:47.686511
+Create Date: 2022-06-05 20:37:30.957122
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = 'ef015387aa5a'
+revision = '6f871b0810aa'
 down_revision = None
 branch_labels = None
 depends_on = None
