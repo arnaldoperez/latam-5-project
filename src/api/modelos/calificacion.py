91870cb6d87b327ae0a0e57ec206234b451feb7d
Arnaldo Perez <arnaldoeperezb@yahoo.com>
2022-06-18 01:14:36 +0000

    Modelos separados en archivos

---
+++ src/api/modelos/calificacion.py
@@ -0,0 +1,21 @@
+from ..db import db
+
+class Calificacion(db.Model):
+    __tablename__ = 'calificacion'
+    id = db.Column(db.Integer, primary_key=True)
+    calificacion = db.Column(db.String(50), nullable=False)
+    comentario = db.Column(db.String(250), nullable=False)
+    id_tecnico = db.Column(db.Integer, db.ForeignKey('perfil_tecnico.id'))
+    propuesta_id = db.Column(db.Integer, db.ForeignKey('propuesta.id'))
+    fecha_cierre= db.Column(db.String(10), nullable=False)
+    
+
+    def serialize(self):
+        return {
+            'id': self.id,           
+            'calificacion': self.calificacion,
+            'comentario': self.comentario,
+            'id_tecnico': self.id_tecnico,
+            'propuesta_id': self.propuesta_id,
+            'fecha_cierre': self.fecha_cierre
+        }
---
+++ src/api/modelos/perfilTecnico.py
@@ -0,0 +1,30 @@
+from ..db import db
+
+class Perfil_tecnico(db.Model):
+
+    __tablename__ = 'perfil_tecnico'
+
+    id = db.Column(db.Integer, primary_key=True)
+    historial = db.Column(db.String(120), nullable=False)
+    ubicacion = db.Column(db.String(80), nullable=False)
+    descripcion = db.Column(db.String(120), nullable=False)
+    url = db.Column(db.String(120), nullable=False)
+    id_user = db.Column(db.Integer, db.ForeignKey('user.id'))
+    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    propuesta = db.relationship('Propuesta',lazy=True, backref='perfil_tecnico')
+    calificacion = db.relationship('Calificacion', backref='perfil_tecnico')
+  
+    def __repr__(self):
+        return f'<User {self.historial}>'
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "historial": self.historial,
+            "ubicacion": self.ubicacion,
+            "descripcion": self.descripcion,
+            "url": self.url,
+            "fecha_ing": self.fecha_ing,
+            "id_user":self.id_user, 
+            "is_active":self.is_active
+        }
